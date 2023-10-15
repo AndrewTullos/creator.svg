a43c513c6550307131e523b5253ec9e3c0c06c1b
Drew Tullos <tullos.andrew@gmail.com>
2023-10-14 20:01:06 -0500

    skeleton outline

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const fs = require('fs');
 const inquirer = require ('inquirer');
 const path = require('path')
+const generateLogo = require('./lib/shapes')
 
+// Inquirer questions to genrerate the image
 const questions = [
     {
         type: 'input',
@@ -26,3 +28,22 @@ const questions = [
         message: 'Enter the shape\'s color (Use a color keyword or a hexadecimal number).',
     },
 ]
+
+// // // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+        }
+
+// // TODO: Create a function to initialize app
+function init() {
+    // Create a README inside of folder 'dist'
+    fs.mkdir('dist', (err) => {
+        if (err) throw err;
+    });
+
+inquirer.prompt(questions).then((answers) => {
+    console.log("Creating your custom SVG Logo!");
+    writeToFile("./dist/Logo.svg", generateLogo({ ...answers }));
+    });
+}
+init();
